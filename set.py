e54f1d8b8515fec488f86a2bb579f55771844341
Sara Mahdavi <saramahdavi146@gmail.com>
2025-04-19 11:50:39 +0330

    secound commit for set Data Type

--- set.py
+++ set.py
@@ -7,7 +7,7 @@ sampleSet = {1, 'A', 'g', 3.6, 'Dayche'}
 
 for _ in range(10):
     print(sampleSet)
-print('#' * 40)
+
 
 odd = set(range(1,50, 2))
 power = {1, 4, 9, 16, 25, 36,  49}
@@ -20,3 +20,11 @@ print(odd)
 
 
 power_frozen = frozenset(power)
+
+
+
+
+
+
+
+
